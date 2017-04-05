645749ad9374189d9ab752d58a9304065367b87e
goikomikel23 <uppa@MacBook-Pro-de-Mikel.local>
2017-04-05 16:45:15 +0200

    Commit mofications Alejandro

--- Avatars/resources/views/insertAvatarForm.blade.php
+++ Avatars/resources/views/insertAvatarForm.blade.php
@@ -3,16 +3,33 @@
 @section('contentForm')
 
 
-                    {!! Form::open(['route' => 'addAvatarSubmit', 'method' => 'post', 'files' => true]) !!}
-                    {!! Form::label('emailLabel_form', 'Email') !!}
-                    {!! Form::text('email_form', '') !!}
-                    <br/>
-                    {!! Form::label('avatarLabel_form', 'Avatar') !!}
-                    {!! Form::file('image') !!}
+    {!! Form::open(['route' => 'addAvatarSubmit', 'method' => 'post', 'files' => true]) !!}
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                {!! Form::label('emailLabel_form', 'Email') !!}
+                {!! Form::email('email_form', '') !!}
 
-                    <br/>
-                    {!! Form::submit('Submit') !!}
+            </div>
+        </div>
 
-                    {!! Form::close() !!}
+        <div class="col-xs12 col-sm-12 col-md-12">
+            <div class="form-group">
+                {!! Form::label('avatarLabel_form', 'Avatar') !!}
+                {!! Form::file('image') !!}
+
+            </div>
+        </div>
+
+        <div class="col-xs12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary">Submit</button>
+
+                {!! Form::close() !!}
+            </div>
+
+        </div>
+    </div>
+
 
 @endsection
